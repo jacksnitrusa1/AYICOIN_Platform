c32c4d0595dc513ccd1aca7391a3ff6d9adf2562
unknown <union_000@outlook.com>
2026-06-04 02:10:00 -0700

    Fix hardcoding

--- frontend/components/TokenDeployer/TokenDeployer.jsx
+++ frontend/components/TokenDeployer/TokenDeployer.jsx
@@ -104,7 +104,7 @@ export const TokenDeployer = () => {
       // Use the first Hardhat account (index 0) to send ETH
       const hardhatProvider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
       const hardhatSigner = new ethers.Wallet(
-        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+        "zzzzzzzzzzzzzzzzzzz",
         hardhatProvider
       );
 
